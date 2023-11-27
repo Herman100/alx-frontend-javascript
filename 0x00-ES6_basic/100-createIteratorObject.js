5b4ee59ea8dc182d27e2ffae81574ea0f1768b65
Herman100 <hermankwamebour30@gmail.com>
2023-11-27 23:51:13 +0000

    advanced

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  let employees = [];
+  for (let department in report.allEmployees) {
+    employees = [...employees, ...report.allEmployees[department]];
+  }
+  return employeesSymbol.iterator;
+}
