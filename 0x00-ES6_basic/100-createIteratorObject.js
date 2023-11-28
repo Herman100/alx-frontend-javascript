9ee8cff732efda8d538500fb4b080660f56b9d82
Herman100 <hermankwamebour30@gmail.com>
2023-11-28 05:00:08 +0000

    updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,10 @@
+/* eslint-disable */
 export default function createIteratorObject(report) {
-  let employees = [];
-  for (let department in report.allEmployees) {
-    employees = [...employees, ...report.allEmployees[department]];
+  let employee = [];
+  for (let [departments, employees] of Object.entries(report.allEmployees)) {
+    for (let an_employee of employees) {
+      employee.push(an_employee);
+    }
   }
-  return employeesSymbol.iterator;
+  return employee;
 }
