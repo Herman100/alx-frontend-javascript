61871d800b0182669c58a9e028d84f72accbaa1a
Herman100 <hermankwamebour30@gmail.com>
2023-11-29 12:38:25 +0000

    user final

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,18 @@
 /* eslint-disable */
+
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.value || result.reason,
+    }));
+  });
+}
+
+export default handleProfileSignup;
