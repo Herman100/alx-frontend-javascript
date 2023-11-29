34bfc9e6c3b0eb5a562a6cc3cb535242e09e56c3
Herman100 <hermankwamebour30@gmail.com>
2023-11-29 12:15:41 +0000

    then js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log("Got a response from the API");
+      return { status: 200, body: "success" };
+    })
+    .catch(() => new Error());
+}
+
+export default handleResponseFromAPI;
