b856da4efed3b6fd2ca6799ec24e33458136f7ec
Herman100 <hermankwamebour30@gmail.com>
2023-11-29 13:17:15 +0000

    array updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,15 +4,15 @@ import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
 function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.value || result.reason,
-    }));
-  });
+  const resultArray = [];
+
+  const user = signUpUser(firstName, lastName);
+  resultArray.push({ status: "fulfilled", value: user });
+
+  const upload = uploadPhoto(fileName);
+  resultArray.push({ status: "fulfilled", value: upload });
+
+  return resultArray;
 }
 
 export default handleProfileSignup;
