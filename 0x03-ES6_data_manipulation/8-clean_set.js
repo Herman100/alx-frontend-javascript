78fc4a3eae4f35e030693fd663e760c435b1c90b
Herman100 <hermankwamebour30@gmail.com>
2023-12-05 13:37:33 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
-const cleanSet = (set, startString) => {
-
-  if (!startString || !startString.trim()) {
-    return '';
-  }
-  const results = [];
-  for (const item of set) {
-    if (item && item.startsWith(startString)) {
-      results.push(item.slice(startString.length));
+/* eslint-disable */
+function cleanSet(set, startString) {
+    if (!startString || !startString.trim()) {
+        return '';
     }
-  }
-  return results.join('-');
-};
+    let result = '';
+    for (const item of set) {
+        if (item && item.startsWith(startString)) {
+            result += `${item.slice(startString.length)}-`;
+        }
+    }
+    return result.slice(0, -1);
+}
 
 export default cleanSet;
