51972ce68536b644f409911e88d42cfb6a520483
Herman100 <hermankwamebour30@gmail.com>
2023-12-05 01:11:02 +0000

    more mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+function getListStudentIds(list) {
+    if (!Array.isArray(list)) {
+        return [];
+    }
+    return list.map(student => student.id);
+}
+
+export default getListStudentIds;
