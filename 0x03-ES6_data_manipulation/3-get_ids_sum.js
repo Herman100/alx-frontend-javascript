1240f2e59840f90ee523b8e0e10c9a6feac78954
Herman100 <hermankwamebour30@gmail.com>
2023-12-05 02:42:14 +0000

    reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+function getStudentIdsSum(list) {
+    if (!Array.isArray(list)) {
+        return [];
+    }
+    return list.reduce((total, student) => total + student.id, 0);
+}
+
+export default getStudentIdsSum;
