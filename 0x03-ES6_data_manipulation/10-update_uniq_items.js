1eb14bd96c3d0b772b6477c9e9b08f9e88eda342
Herman100 <hermankwamebour30@gmail.com>
2023-12-05 13:44:49 +0000

    more map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const item of map) {
+    if (item[1] === 1) {
+      map.set(item[0], 100);
+    }
+  }
+};
+
+export default updateUniqueItems;
