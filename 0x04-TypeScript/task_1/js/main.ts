2e6057be98d5cac8b58ba9865242f1ed1ff8c50c
Herman100 <hermankwamebour30@gmail.com>
2023-12-06 19:26:31 +0000

    teacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -36,3 +36,18 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher("Jon", "Doe"));
+
